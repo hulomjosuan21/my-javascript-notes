5b6037cc4f625b0f35c66b72ba74521de8fc587e
Josuan <hulomjosuan@gmail.com>
2024-08-13 15:07:03 +0800

    .

--- src/pages/NotesContainer.jsx
+++ src/pages/NotesContainer.jsx
@@ -18,11 +18,11 @@ const NotesContainer = () => {
 
   return (
     <section className='notes__container' id='notes'>
-      <h1 className='onShowAnimate'>Total Notes <span>({notes.length})</span></h1>
+      <h1 className='onShowAnimate'>{(notes.length > 0) ? `Total Notes ${<span>{notes.length}</span>}` : "Loading..."}</h1>
 
       <div className="notes_wrapper">
         {
-          notes.map(({id, title, code, description}) => (
+          notes && notes.map(({id, title, code, description}) => (
             <CodeBoxOne key={id} title={title} code={code} description={description}/>
           ))
         }
